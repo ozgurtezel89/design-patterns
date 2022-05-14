86224a852447d84d665c1565d7de5b1c4b120f5f
ozgurtezel89 <ozgurtezel89@gmail.com>
2022-05-14 03:37:52 +0100

    Corrected namespace Added logic for factorymethod creational pattern

--- src/Creational.FactoryMethod/Program.cs
+++ src/Creational.FactoryMethod/Program.cs
@@ -2,12 +2,62 @@
 
 using System;
 
-namespace HelloWorld;
+namespace Creational.FactoryMethod;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Application Started!");
+
+        var roadLogistic = new RoadLogistic();
+        var transport = roadLogistic.CreateTransport();
+        transport.Deliver();
+
+        var seaLogistic = new SeaLogistic();
+        var transport2 = seaLogistic.CreateTransport();
+        transport2.Deliver();
+    }
+}
+
+interface ILogistic
+{
+    ITransport CreateTransport();
+}
+
+class RoadLogistic : ILogistic
+{
+    public ITransport CreateTransport()
+    {
+        return new Truck();
+    }
+}
+
+class SeaLogistic : ILogistic
+{
+    public ITransport CreateTransport()
+    {
+        return new Ship();
+    }
+}
+
+interface ITransport
+{
+    void Deliver();
+}
+
+class Truck : ITransport
+{
+    public void Deliver()
+    {
+        Console.WriteLine($"Delivering via {nameof(Truck)}");
+    }
+}
+
+class Ship : ITransport
+{
+    public void Deliver()
+    {
+        Console.WriteLine($"Delivering via {nameof(Ship)}");
     }
 }
